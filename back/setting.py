2d4f929b1bdfd9b5efe790c8968db5a150713ab5
lichangan <13132208596@163.com>
2021-01-31 16:09:15 +0800

    更改了一部分配置

--- back/setting.py
+++ back/setting.py
@@ -16,7 +16,7 @@ INITIAL_POST_IDENTIFIER = 19930126
 TEMPORARY_PW_EXPIRE_MINUTES = 10  # min
 TEMPORARY_PW_EXPIRE_SECONDS = 60 * TEMPORARY_PW_EXPIRE_MINUTES  # second
 # user info
-SEND_BACKUP_TO_MAIL = 'emailme8@163.com'        # TODO：用户备份邮箱
+SEND_BACKUP_TO_MAIL = 'emailme8@163.com'  # TODO：用户备份邮箱
 # fp
 APP_LOG_FP = 'logs/app.log'
 BACK_UP_DATA_FP = 'back/data/backup'
@@ -25,18 +25,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 # flask
 FLASK_HOST = '0.0.0.0'
-FLASK_PORT = '5000'
+FLASK_PORT = '8888'
 HOST_IP = os.getenv('FLASK_HOST', FLASK_HOST)
 # baidu_trans
 BD_TRANS_API_URL = '/api/trans/vip/translate'
 # db
 DB_HOST = 'localhost'
-DB_USER = 'root'            # TODO:export this
+DB_USER = 'root'  # TODO:export this
 DB_PASSWORD = '111111'
 config_name = os.getenv('FLASK_CONFIG', 'default')
-print(config_name,'----------------config_name')
+print(config_name, '----------------config_name')
 DATABASE_NAME = 'iyblog_dev'
-print(DATABASE_NAME,'-------DATABASE_NAME---------config_name')
+print(DATABASE_NAME, '-------DATABASE_NAME---------config_name')
 # regex
 RE_SYMBOL = r'[\,\，\.\。\?\？\:\：\'\‘\’\"\“\”\、\/\*\&\$\#\@\!\(\（\)\）\[\【\]\】\{\}\|\-"]'
 RE_EXCLUDE_CHINESE = r'[A-Za-z0-9\!\%\[\]\,\。]'
--- runserver.py
+++ runserver.py
@@ -8,4 +8,5 @@ app = create_app(os.getenv('FLASK_CONFIG', 'default'))
 
 if __name__ == '__main__':
     host_ip = setting.HOST_IP
-    app.run(host=host_ip)  # TODO:此处需要写进环境变量
+    port = setting.FLASK_PORT
+    app.run(host=host_ip, port=port)  # TODO:此处需要写进环境变量
